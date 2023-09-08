3cc4b571e682613a6423faef1417d5db2fb6937c
mkumm14 <madhavkummara@outlook.com>
2023-09-08 01:03:01 -0500

    removed print statements

--- faultfinder/settings.py
+++ faultfinder/settings.py
@@ -31,14 +31,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('DJANGO_SECRET_KEY')
 
-print(SECRET_KEY)
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 
-print(DEBUG)
 
 ALLOWED_HOSTS = []
 
