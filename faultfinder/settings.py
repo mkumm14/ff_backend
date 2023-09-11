ae1d1dbcb5bf888237637b98bdc6ce6243d77916
mkumm14 <madhavkummara@outlook.com>
2023-09-11 00:44:18 -0500

    add changes

--- faultfinder/settings.py
+++ faultfinder/settings.py
@@ -23,6 +23,9 @@ from decouple import config
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
+# adding changes 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
