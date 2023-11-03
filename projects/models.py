17e8d6ecac18ab3c3fe02a210197ae6358f0d1de
mkumm14 <madhavkummara@outlook.com>
2023-11-03 12:17:27 -0500

    change id field on project model ot uuid instead of int

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -5,6 +6,7 @@ from django.contrib.auth.models import User
 
 
 class Project(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title=models.CharField(max_length=50)
     description=models.TextField()
     owner=models.ForeignKey(User, on_delete=models.CASCADE)
