a674818da16ea1662fe5e9af95a42cfb96a52a54
mkumm14 <madhavkummara@outlook.com>
2023-11-13 19:47:19 -0600

    test commit

--- faultfinder/settings.py
+++ faultfinder/settings.py
@@ -20,6 +20,8 @@ from decouple import config, Csv
 import dj_database_url
 
 
+#This is from fault-finder repo
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
