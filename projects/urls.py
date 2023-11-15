c57124d405f2031708a7327ef1c5b27933d93511
mkumm14 <madhavkummara@outlook.com>
2023-11-15 16:39:22 -0600

    remove uuid field

--- projects/urls.py
+++ projects/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('user-projects', views.UserProjectView.as_view(), name='user-projects'),
     path('create', views.create_project, name="create-project"),
     path('update/<uuid:pk>', views.update_project, name='update-project'),
-    path('<uuid:pk>', views.ProjectDetailsView.as_view(), name="project-details")
+    path('<int:pk>', views.ProjectDetailsView.as_view(), name="project-details")
 ]
