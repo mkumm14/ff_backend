13c5ac136154e69b294559398f2212743dfdce82
mkumm14 <madhavkummara@outlook.com>
2024-01-10 22:01:59 -0600

    add docker file and set up database

--- faultfinder/settings.py
+++ faultfinder/settings.py
@@ -112,12 +112,10 @@ WSGI_APPLICATION = "faultfinder.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
-        "USER": os.environ.get("SQL_USER", "user"),
-        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
-        "HOST": os.environ.get("SQL_HOST", "localhost"),
-        "PORT": os.environ.get("SQL_PORT", "5432"),
+        dj_database_url.config(
+            conn_max_age=600,
+            conn_health_checks=True,
+        )
     }
 }
 
