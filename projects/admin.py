629c217eab912e3d1d765fa25c85e0719d4d5faa
mkumm14 <madhavkummara@outlook.com>
2024-10-18 01:34:11 -0500

    update server documentation

--- projects/admin.py
+++ projects/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
+"""
+This module registers the Project model with the Django admin site.
+Imports:
+    from django.contrib import admin: Imports the Django admin module.
+    from projects.models import Project: Imports the Project model from the projects app.
+Functionality:
+    Registers the Project model with the Django admin site, allowing it to be managed through the admin interface.
+"""
 
 from projects.models import Project
 
@@ -6,3 +14,4 @@ from projects.models import Project
 
 
 admin.site.register(Project)
+
